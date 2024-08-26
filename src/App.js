788922c78e999e55456d089133ddfdee8dfa12ba
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-08-26 21:44:05 +0800

    07 JSX基础 JSX中实现复杂条件渲染

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-const isLogin = false
+const articleType = 1 // 0 1 3
+
+// 定义核心函数（根据文章类型返回不同的JSX模板）
+function getArticleTem() {
+    if (articleType === 0) {
+        return <div>我是无图文章</div>
+    } else if (articleType === 1) {
+        return <div>我是单图模式</div>
+    } else if (articleType === 3) {
+        return <div>我是三图模式</div>
+    }
+}
 
 function App() {
     return (<div className="App">
         this is app
-        {/*逻辑与 &&*/}
-        {isLogin && <span>this is span</span>}
-
-        {/*三元运算*/}
-        {isLogin ? <span>jack</span> : <span>loading...</span>}
+        {/*调用函数渲染不同的模板*/}
+        {getArticleTem()}
     </div>);
 }
 
