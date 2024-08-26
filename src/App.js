f3c0040a1366357c76e73e8e20abdf7496edf86c
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-08-26 22:41:51 +0800

    09 React组件基础使用

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
+// 1.定义组件
+function Button() {
+    return <button>click me!</button>
+}
+
 function App() {
-    // 基础绑定
-    // const handleClick = () => {
-    //     console.log("button被点击了")
-    // }
-    // return (<div className="App">
-    //     <button onClick={handleClick}>click me</button>
-    // </div>);
-
-    // 事件绑定e
-    // const handleClick = (e) => {
-    //     console.log("button被点击了", e)
-    // }
-    //
-    // return (<div className="App">
-    //     <button onClick={handleClick}>click me</button>
-    // </div>);
-
-    // 传递自定义参数
-    // const handleClick = (name) => {
-    //     console.log("button被点击了", name)
-    // }
-    // return (<div className="App">
-    //     <button onClick={() => handleClick('jack')}>click me</button>
-    // </div>);
-
-    // 既传递自定义参数，又传递事件对象ane
-    const handleClick = (name, e) => {
-        console.log("button被点击了", name, e)
-    }
     return (<div className="App">
-        <button onClick={(e) => handleClick('jack', e)}>click me</button>
+        {/*2.使用组件（渲染组件）*/}
+        {/*自闭和*/}
+        <Button/>
+        {/*成对标签*/}
+        <Button></Button>
     </div>);
 }
 
