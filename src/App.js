9a031c622a836acba9791f0a59ff194b9d30c23d
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-08-26 21:36:03 +0800

    06 JSX基础 JSX中实现基础条件渲染

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-const list = [{id: 1001, name: 'Vue'}, {id: 1002, name: 'React'}, {id: 1003, name: 'Angular'},]
+const isLogin = false
 
 function App() {
     return (<div className="App">
         this is app
-        {/*渲染列表*/}
-        {/*map循环哪个结构 就return哪个结构*/}
-        {/*注意事项：加上一个独一无二的key 字符串或者number id*/}
-        {/*key的作用：React框架内部使用 提升更新性能*/}
-        <ul>
-            {list.map(item => <li key={item.id}>{item.name}</li>)}
-        </ul>
+        {/*逻辑与 &&*/}
+        {isLogin && <span>this is span</span>}
+
+        {/*三元运算*/}
+        {isLogin ? <span>jack</span> : <span>loading...</span>}
     </div>);
 }
 
