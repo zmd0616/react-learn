c4bfa3f3d99e645bfaabda75c582b6b357ef7f4b
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-08-27 13:25:35 +0800

    12 基础样式控制

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import {useState} from "react";
+// 导入样式
+import './index.css'
+const styles = {
+    color: 'red',
+    fontSize: '50px'
+}
 
 function App() {
-    let [count, setCount] = useState(0);
-
-    function handleClick() {
-        // 直接修改，无法引发视图更新
-        count++
-    }
-
-    // 修改对象状态
-    const [form, setForm] = useState({name: 'jack'})
-    const changeForm = () => {
-        // 错误写法：直接修改
-        // form.name = 'john'
-
-        // 正确写法：setForm传入一个全新的对象
-        setForm({
-            ...form,
-            name: 'john'
-        })
-    }
-
     return (<div className="App">
-        <button onClick={handleClick}>{count}</button>
-        <button onClick={changeForm}>changeForm:{form.name}</button>
+        <span style={styles}>this is span</span>
+        <span className="foo">this is class foo</span>
     </div>);
 }
 
