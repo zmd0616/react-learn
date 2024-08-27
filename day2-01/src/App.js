ccc74cbaa006dbd519e7ec37c8ca7a081c78d2a5
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-08-27 21:56:33 +0800

    Day2-01 表单受控绑定

--- day2-01/src/App.js
+++ day2-01/src/App.js
@@ -1,14 +1,17 @@
-// 导入样式
-import './index.css'
-const styles = {
-    color: 'red',
-    fontSize: '50px'
-}
+import { useState } from "react";
+
+// 核心绑定流程
+// 1.通过value属性绑定react状态
+// 2.绑定onChange事件 通过事件参数e拿到输入框最新的值 反向修改到react状态
 
 function App() {
+    const [value, setValue] = useState('')
     return (<div className="App">
-        <span style={styles}>this is span</span>
-        <span className="foo">this is class foo</span>
+        <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+        ></input>
     </div>);
 }
 
