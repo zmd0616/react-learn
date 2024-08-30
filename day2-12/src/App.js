9355b673eb0fee32b9ab5295a8357530f2d48835
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-08-30 11:58:34 +0800

    Day2-13.useEffect-不同依赖项说明

--- day2-12/src/App.js
+++ day2-12/src/App.js
@@ -1,10 +1,26 @@
-import { useEffect, useEffect } from 'react'
+import {useEffect, useState} from 'react'
 
 function App() {
+    const [count1, setCount1] = useState(0)
+    const [count2, setCount2] = useState(0)
+    // 1.没有依赖项，组件初始化渲染+组件更新时执行
+    // useEffect(() => {
+    //     console.log('副作用函数执行了')
+    // })
+
+    // 2.空数组依赖，只在组件初始化渲染时执行一次
+    // useEffect(() => {
+    //     console.log('副作用函数执行了')
+    // }, [])
+
+    // 3.添加特定依赖项，组件初始化渲染+特定依赖项发生变化时候执行
     useEffect(() => {
         console.log('副作用函数执行了')
-    })
+    }, [count1])
+
     return (<div className="App">
+        <button onClick={() => setCount1(count1 + 1)}>button1:{count1}</button>
+        <button onClick={() => setCount2(count2 + 1)}>button2:{count2}</button>
     </div>);
 }
 
