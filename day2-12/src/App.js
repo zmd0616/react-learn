72ec73e12485f1eda4bed5e36fb31f8a3d708b1e
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-08-30 11:04:13 +0800

    Day2-12 useEffect-概念理解和基础使用

---
+++ day2-12/src/App.js
@@ -0,0 +1,11 @@
+import { useEffect, useEffect } from 'react'
+
+function App() {
+    useEffect(() => {
+        console.log('副作用函数执行了')
+    })
+    return (<div className="App">
+    </div>);
+}
+
+export default App;
