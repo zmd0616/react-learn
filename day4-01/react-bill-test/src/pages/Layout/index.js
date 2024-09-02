0692f1ae3041127ec1072dbb44945dfc2d1625f1
ZMD-PC\zmd0616 <zmd0616@outlook.com>
2024-09-02 23:16:02 +0800

    04完结， 实现TabBar

--- day4-01/react-bill-test/src/pages/Layout/index.js
+++ day4-01/react-bill-test/src/pages/Layout/index.js
@@ -1,22 +1,55 @@
-import { Outlet } from 'react-router-dom';
-import { Button } from 'antd-mobile';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Button, TabBar } from 'antd-mobile';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getBillList } from '@/store/modules/billStore';
+import './index.scss';
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from 'antd-mobile-icons';
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+];
 
 const Layout = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBillList());
   }, [dispatch]);
+  // 切换菜单跳转路由
+  const navigate = useNavigate();
+  const swithRoute = (path) => {
+    console.log(path);
+    navigate(path);
+  };
   return (
-    <div>
-      <Outlet />
-      我是Layout
-      {/*测试全局生效样式*/}
-      <Button color="primary">测试全局</Button>
-      <div className="puple">
-        <Button color="primary">测试局部</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={(path) => swithRoute(path)}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
       </div>
     </div>
   );
